5f9aae42bfe1db1f7af8a4096ac8559c48ace3d0
Justin <juswerth@gmx.de>
2026-03-14 22:26:23 +0100

    Updated DB URI for superset to use its own schema

--- setup/superset/superset_config.py
+++ setup/superset/superset_config.py
@@ -1,2 +1,4 @@
 
 SECRET_KEY="+tGM8hH8DwsF9q9Om4XAiMm7SNDmxlrdPIy3H7ER0hTT087KsiegW"
+
+SQLALCHEMY_DATABASE_URI ="postgresql://superset_user:superset_password@postgres:5432/redditor_db?options=-csearch_path=superset"
