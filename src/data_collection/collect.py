33fbdaed42773b080797026b45e9cc6a4c49ac6d
Justin <juswerth@gmx.de>
2026-03-16 18:40:36 +0100

    Added function to retrieve API secret in code.

--- src/data_collection/collect.py
+++ src/data_collection/collect.py
@@ -1,5 +1,13 @@
 from time import sleep
 from os import environ
+from functools import cache
+
+# Read the Reddit API secret
+@cache
+def get_api_secret():
+    with open("/run/secrets/reddit_api_secret", "r") as f:
+        return f.read().strip()
+
 
 # Mock for data collection function
 def collect_data():
@@ -7,6 +15,7 @@ def collect_data():
     while True:
         print("Data Collection Mock.")
         print(f"Variable test: {(t := environ.get('DC__DEV__TEST_VAR'))} ({bool(t)})")
+        print(f"Secret: {bool(get_api_secret())}, len={len(get_api_secret())}")
         print(end="", flush=True)
         sleep(5)
 
