fcf38223ab5ba3f3b5ec6d6847a90075a24fb422
Justin <juswerth@gmx.de>
2026-04-04 13:55:06 +0200

    Main loop for data processor

--- src/data_processing/main.py
+++ src/data_processing/main.py
@@ -1,11 +1,28 @@
+# User module imports
+import constants as c
+from sample_processor import SampleProcessor
+
 # Python module imports
 from time import sleep
 
 
 def process_data():
+
+    # Get sample processor
+    sample_processor = SampleProcessor.load()
+
+    # Wait for initialization
+    sample_processor.wait_for_data_lake()
+
+    # Main loop
     while True:
-        print("Mocking data processing...", flush=True)
-        sleep(5)
+        # Check for unprocessed samples
+        if not sample_processor.data_lake_has_unprocessed_files():
+            sleep(c.DATA_LAKE_REFRESH_TIME)
+            continue
+
+        # Update all unprocessed samples
+        sample_processor.update_database()
 
 
 if __name__ == "__main__":
